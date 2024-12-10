ad0f1c22140cb02600066d45dcda5c78df1d2db8
faithfajin <faithfrew21@gmail.com>
2024-12-11 00:21:43 +0700

    Add: Homepage

--- app/src/main/java/com/bangkit/subur/MainActivity.kt
+++ app/src/main/java/com/bangkit/subur/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity(){
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
         binding.fab.setOnClickListener {
             openFragment(RicePlantDetectorFragment())
             binding.bottomNavigation.selectedItemId = R.id.navigation_riceplantdetector
